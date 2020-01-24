be7281df3439f685ab3e59dee1d8bbe7da5f5533
danielmengesha <daniel.mengesha@gmx.de>
2020-01-24 11:22:18 +0100

    set up project enviroment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//import logo from './logo.svg';
+import './App.css';
+
+//import Routes from './routes';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>hallo </h1>
+    </div>
+  );
+};
+
+export default App;
